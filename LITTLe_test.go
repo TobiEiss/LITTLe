749bd7657f940be12f47acaeb7a89f2d2e3233a0
TobiEiss <eiss.tobias@googlemail.com>
2017-08-11 11:35:39 +0200

    create test for testCase

--- LITTLe_test.go
+++ LITTLe_test.go
@@ -34,3 +34,34 @@ func TestATestStep(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestATestCase(t *testing.T) {
+	// create test-server
+	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+	}))
+	defer testServer.Close()
+
+	// create TestStep
+	testStep := LITTLe.TestStep{}
+	request, err := http.NewRequest("GET", testServer.URL, nil)
+	if err != nil {
+		log.Println(err)
+		t.Fail()
+	}
+	testStep.ExpectedStatus = 200
+	testStep.Request = request
+
+	// create TestCase
+	testCase := LITTLe.TestCase{}
+	testCase.Before = []LITTLe.TestUnit{testCase}
+	testCase.TestUnits = []LITTLe.TestUnit{testCase}
+	testCase.After = []LITTLe.TestUnit{testCase}
+
+	// run testCase
+	err = testCase.Run()
+	if err != nil {
+		log.Println(err)
+		t.Fail()
+	}
+}
